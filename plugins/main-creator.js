c6558a2d025317193a1d584b0a20d707ce59132c
Niño Piña <ninopina10@gmail.com>
2024-11-29 16:49:00 -0600

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
 let txt_owner = "> _*`Hola!,Fui Diseñado Para La Administración y Gestión De Grupos, Este Es El Número De Mi Creador`*_\n\n *David Alexander* : Wa.me/18297448661"
-await conn.sendFile(m.chat, "https://files.catbox.moe/ge77oy.jpg", 'thumbnail.jpg', txt_owner, m, null, rcanal)
+await conn.sendFile(m.chat, "https://qu.ax/nPMCF.jpg", 'thumbnail.jpg', txt_owner, m, null, rcanal)
 }
 handler.help = ['owner']
 handler.tags = ['main']
